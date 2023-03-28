922265ba89c2c2a86961628a8f12b386331f13fe
Christina Niederer <christina-niederer@gmx.at>
2023-03-28 18:29:12 +0200

    Send Data from input to board mask

--- Go_Gruppe1/src/main/java/com/example/go_gruppe2/boardMaskController.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe2/boardMaskController.java
@@ -1,5 +1,44 @@
 package com.example.go_gruppe2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+
 public class boardMaskController {
 
+    @FXML
+    private Label pl1, pl2, sizeOfBoard, komiBoard, handicapsBoard;
+    public void displayPlayerNames(String p1, String p2) {
+        if(p1.isEmpty()) {
+            if(!p2.equals("Player 1")); //names cannot be the same
+            p1 = "Player 1";
+        }
+        if(p2.isEmpty()) {
+            if(!p1.equals("Player 2")); //names cannot be the same
+            p2 = "Player 2";
+        }
+
+        pl1.setText("Black is: " + p1);
+        pl2.setText("White is: " + p2);
+    }
+
+    public void displayBoardSize(String size) {
+        sizeOfBoard.setText("Boardsize: " + size + "x" + size);
+    }
+
+    public void displayKomi(String komiAdvantage) {
+        komiBoard.setText("Komi: 0");
+
+        if(Integer.valueOf(komiAdvantage) > 0) {
+            komiBoard.setText("Komi: " + komiAdvantage);
+        }
+    }
+
+    public void displayHandicaps(String handicaps) {
+        handicapsBoard.setText("Handicaps: 0");
+
+        if(Integer.valueOf(handicaps) > 0) {
+            handicapsBoard.setText("Handicaps: " + handicaps);
+        }
+    }
 }
--- Go_Gruppe1/src/main/java/com/example/go_gruppe2/inputMaskController.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe2/inputMaskController.java
@@ -6,10 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,24 +17,42 @@ public class inputMaskController {
     private Scene scene;
     private Parent root;
     @FXML
-    private Label boardSizeLabel;
+    private Label boardSizeLabel, komiLabel, handicapsLabel, timeRuleLabel;
 
     @FXML
     private RadioButton size9, size11, size13, size19;
 
     @FXML
+    private ToggleGroup boardSize;
+
+    @FXML
     private Button startButton;
 
     @FXML
-    private TextField player1, player2;
+    private TextField player1, player2, komi, handicaps;
 
     public void switchToBoardMask(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("boardMaskGUI.fxml"));
+        String p1 = player1.getText();
+        String p2 = player2.getText();
+        int size = getBoardSize(new ActionEvent());
+        int komiAdvantage = Integer.parseInt(komi.getText());
+        int handicapAdvantage = Integer.parseInt(handicaps.getText());
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("boardMaskGUI.fxml"));
+        root = loader.load();
+
+        boardMaskController boardMask = loader.getController();
+        boardMask.displayPlayerNames(p1, p2);
+        boardMask.displayBoardSize(String.valueOf(size));
+        boardMask.displayKomi(String.valueOf(komiAdvantage));
+        boardMask.displayHandicaps(String.valueOf(handicapAdvantage));
+
+
+        //Parent root = FXMLLoader.load(getClass().getResource("boardMaskGUI.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
     }
 
     public void switchToInputMask(ActionEvent event) throws IOException {
