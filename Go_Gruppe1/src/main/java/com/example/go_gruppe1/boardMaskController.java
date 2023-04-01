7d5acf4740e3d3ad50bf1b6e4a62ebd6f1d00d7b
Christina Niederer <christina-niederer@gmx.at>
2023-04-01 20:58:45 +0200

    Resizability and Board Mask changes
    logical and layout changes in both GUI masks

--- Go_Gruppe1/src/main/java/com/example/go_gruppe1/boardMaskController.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe1/boardMaskController.java
@@ -1,15 +1,34 @@
 package com.example.go_gruppe1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ToggleGroup;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class boardMaskController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private GridPane boardPane;
     @FXML
-    private Label pl1, pl2, sizeOfBoard, komiBoard, handicapsBoard;
+    private Label pl1, pl2, komiBoard, handicapsBoard, blackTrapped, whiteTrapped;
+    @FXML
+    private ToggleButton play, navigate;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Rectangle board;
     public void displayPlayerNames(String p1, String p2) {
         if(p1.isEmpty()) {
             if(!p2.equals("Player 1")); //names cannot be the same
@@ -20,12 +39,8 @@ public class boardMaskController {
             p2 = "Player 2";
         }
 
-        pl1.setText("Black is: " + p1);
-        pl2.setText("White is: " + p2);
-    }
-
-    public void displayBoardSize(String size) {
-        sizeOfBoard.setText("Boardsize: " + size + "x" + size);
+        pl1.setText(p1 + " (Black)");
+        pl2.setText(p2 + " (White)");
     }
 
     public void displayKomi(String komiAdvantage) {
@@ -45,8 +60,57 @@ public class boardMaskController {
     }
 
     public void setSize(double width, double height) {
-        System.out.println(width + " " + height);
         boardPane.setPrefHeight(height);
         boardPane.setPrefWidth(width);
+    }
+
+    public boolean getMode(ActionEvent event) {
+        if(play.isSelected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void displayBlackTrapped(String black) {
+        blackTrapped.setText("Trapped: 0");
+
+        if((!black.isEmpty() && Integer.valueOf(black)  > 0)) {
+            blackTrapped.setText("Trapped: " + black);
+        }
+    }
+
+    public void displayWhiteTrapped(String white) {
+        whiteTrapped.setText("Trapped: 0");
+
+        if((!white.isEmpty() && Integer.valueOf(white)  > 0)) {
+            blackTrapped.setText("Trapped: " + white);
+        }
+    }
+
+    public double getWidth() {
+        return boardPane.getWidth();
+    }
+
+    public double getHeight() {
+        return boardPane.getHeight();
+    }
+
+    public void switchToInputMask(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/inputMaskGUI.fxml"));
+        root = loader.load();
+
+        inputMaskController inputMask = loader.getController();
+        inputMask.setSize(getWidth(), getHeight());
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void drawBoard(int size) {
+        board.widthProperty().bind(boardPane.widthProperty().multiply(0.42));
+        board.heightProperty().bind(boardPane.widthProperty().multiply(0.42));
     }
 }
