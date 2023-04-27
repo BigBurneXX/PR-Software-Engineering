124529eae733682bc4ca12d48db092858f296677
BigBurneXX <128181584+BigBurneXX@users.noreply.github.com>
2023-04-27 09:48:52 +0200

    Revert "Merge branch 'main' into Changes-by-Ivan"
    This reverts commit e5ef43937c09fb912d408d3a1d3d296ecb46f849, reversing
    changes made to b1fad6b4f6e5a3021ca5aa35571f6cc754cb0cd9.

--- Go_Gruppe1/src/main/java/com/example/go_gruppe1/StoneGroup.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe1/StoneGroup.java
@@ -2,7 +2,6 @@ package com.example.go_gruppe1;
 
 import javafx.scene.paint.Color;
 
- Changes-by-Ivan
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,34 +11,19 @@ public class StoneGroup {
     private final BoardLogicControl boardLogicControl;
     private int liberties = 0;
 
-import java.util.HashSet;
-import java.util.Set;
-
-public class StoneGroup {
-    private final Color colour;
-    private final Set<Position> positions = new HashSet<>();
-    private final Set<Position> freeFields = new HashSet<>();
-    private final BoardLogicControl boardLogicControl;
- main
-
     protected StoneGroup(BoardLogicControl boardLogicControl, Color colour, int row, int col){
         this.boardLogicControl = boardLogicControl;
         this.colour = colour;
         positions.add(new Position(row, col));
     }
 
- Changes-by-Ivan
     protected void addPositions(List<Position> positions){
-
-    protected void addPositions(Set<Position> positions){
- main
         this.positions.addAll(positions);
     }
     protected void addPosition(Position p) {
         positions.add(p);
     }
 
- Changes-by-Ivan
     protected void addPosition(int row, int col){
         positions.add(new Position(row, col));
     }
@@ -61,35 +45,12 @@ public class StoneGroup {
 
     protected void addLiberties(int liberties){
         this.liberties += liberties;
-
-    protected void addFreeField(int row, int col){
-        freeFields.add(new Position(row, col));
-    }
-
-    protected void addFreeField(Position pos){
-        freeFields.add(pos);
-    }
-
-    protected void removeFreeField(Position pos){
-        freeFields.remove(pos);
-        if(freeFields.isEmpty())
-            isDead();
-    }
-
-    protected void addFreeFields(Set<Position> toAdd){
-        freeFields.addAll(toAdd);
-    }
-
-    private void isDead() {
-        boardLogicControl.deleteStone(this);
- main
     }
 
     protected Color getColour() {
         return colour;
     }
 
- Changes-by-Ivan
     protected List<Position> getPosition() {
         return positions;
     }
@@ -98,13 +59,3 @@ public class StoneGroup {
         return liberties;
     }
 }
-
-    protected Set<Position> getPosition() {
-        return positions;
-    }
-
-    protected Set<Position> getFreeFields(){
-        return freeFields;
-    }
-}
- main
