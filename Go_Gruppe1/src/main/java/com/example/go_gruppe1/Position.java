37e672a9e203be1fce7fdb6d1ddeb60dfd7b5528
Bernhard <128181584+BigBurneXX@users.noreply.github.com>
2023-05-04 19:47:35 +0200

    Delete Go_Gruppe1 directory

--- Go_Gruppe1/src/main/java/com/example/go_gruppe1/Position.java
+++
@@ -1,4 +0,0 @@
-package com.example.go_gruppe1;
-
-public record Position(int row, int col) {
-}
--- Go_Gruppe1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.go_gruppe1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.go_gruppe1 to javafx.fxml;
-    exports com.example.go_gruppe1;
-}
