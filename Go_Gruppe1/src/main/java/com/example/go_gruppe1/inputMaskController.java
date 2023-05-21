1afd82018d8ce3a1586a8591a65602e612713118
vasilev14 <ivvveselinov@gmail.com>
2023-05-21 14:02:53 +0300

    Playing with keyboard

--- Go_Gruppe1/src/main/java/com/example/go_gruppe1/inputMaskController.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe1/inputMaskController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
 import java.io.IOException;
 
@@ -47,6 +49,30 @@ public class inputMaskController {
         stage.setMinWidth(600);
         stage.setMinHeight(650);
 
+        // Set up keyboard event handling
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:
+                        boardMask.moveUp();
+                        break;
+                    case DOWN:
+                        boardMask.moveDown();
+                        break;
+                    case LEFT:
+                        boardMask.moveLeft();
+                        break;
+                    case RIGHT:
+                        boardMask.moveRight();
+                        break;
+                    case ENTER:
+                        boardMask.placeStone(boardMask.getCurrentX(), boardMask.getCurrentY());
+                        break;
+                }
+            }
+        });
+
         //for larger boards, the window needs to be bigger
         if(getBoardSize() == 19) {
             stage.setMinHeight(800);
@@ -54,6 +80,7 @@ public class inputMaskController {
 
         stage.show();
     }
+
 
     public int getBoardSize() {
         if (size9.isSelected()) {
