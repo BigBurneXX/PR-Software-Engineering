3ede1f55e3bc85cedf0cb2400d38de171e0c2f6b
Christina Niederer <christina-niederer@gmx.at>
2023-06-05 13:35:33 +0200

    Resizability of all components in input & board bask

--- Go_Gruppe1/src/main/java/com/example/go_gruppe1/controller/winnerMaskController.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe1/controller/winnerMaskController.java
@@ -1,16 +1,19 @@
 package com.example.go_gruppe1.controller;
 
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.OverrunStyle;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,52 +24,90 @@ public class winnerMaskController {
     private GridPane pane;
 
     @FXML
-    private Label name, totalPoints, trapped, extraPoints, extraPointsValue, byoyomi, byoyomiLabel;
+    private Label name, totalPoints, trapped, extraPointsValue, byoyomi;
 
     @FXML
-    private Pane left, right, center, bottom;
+    private ButtonBar buttonBar;
+
+    private Button saveAs, newGame, exit;
 
     private int reasonForWinning = 0;
+
     protected void setReasonForWinning(int reason) {
-        if(reason > 0 && reason <= 3) {
+        if (reason > 0 && reason <= 3) {
             reasonForWinning = reason;
         }
     }
 
-    protected void setName(String winner, String other) {
-        if(reasonForWinning == 1) {
+    protected void initiateDisplay(String winner, String other, long total, int trapped, String komiOrHandicaps,
+                                   double komiOrHandicap, int byoyomi, int boyoyomiNumber, int time) {
+        setName(winner, other);
+        setTotalPoints(total);
+        setTrapped(trapped);
+        setExtraPoints(komiOrHandicaps);
+        setExtraPointsValue(komiOrHandicap);
+        setByoyomi(byoyomi, boyoyomiNumber, time);
+    }
+
+    private void initateButtons() {
+
+    }
+
+    private void setName(String winner, String other) {
+        if (reasonForWinning == 1) {
             name.setText(winner + " won!");
-        } else if(reasonForWinning == 2) {
-            name.setText(other  + " resigned. " + winner + " won!");
+        } else if (reasonForWinning == 2) {
+            name.setText(other + " resigned. " + winner + " won!");
         } else {
             name.setText(other + "'s time is up. " + winner + " won!");
         }
+
+        name.setWrapText(true);
+        name.setTextOverrun(OverrunStyle.CENTER_ELLIPSIS);
+        GridPane.setVgrow(name, Priority.ALWAYS);
+
+        name.styleProperty().bind(Bindings.concat(
+                "-fx-font-size: ", pane.heightProperty().multiply(0.046).asString()
+        ));
     }
 
-    protected void setTotalPoints(long total) {
-        totalPoints.setText(String.valueOf(total));
+    private void setTotalPoints(long total) {
+        totalPoints.setText("Total Points: " + total);
+        totalPoints.styleProperty().bind(Bindings.concat(
+                "-fx-font-size: ", pane.heightProperty().multiply(0.03).asString()
+        ));
     }
 
-    protected void setTrapped(int trapped) {
-        this.trapped.setText(String.valueOf(trapped));
+    private void setTrapped(int trapped) {
+        this.trapped.setText("Trapped: " + trapped);
+        this.trapped.styleProperty().bind(Bindings.concat(
+                "-fx-font-size: ", pane.heightProperty().multiply(0.025).asString()
+        ));
     }
 
-    protected void setExtraPoints(String komiOrHandicaps) {
-        extraPoints.setText(komiOrHandicaps);
+    private void setExtraPoints(String komiOrHandicaps) {
+        extraPointsValue.setText(komiOrHandicaps + " ");
     }
 
-    protected void setExtraPointsValue(double komiOrHandicaps) {
-        extraPointsValue.setText(String.valueOf(komiOrHandicaps));
+    private void setExtraPointsValue(double komiOrHandicaps) {
+        String komiOrHandicap = extraPointsValue.getText();
+        extraPointsValue.setText(komiOrHandicap + komiOrHandicaps);
+        extraPointsValue.styleProperty().bind(Bindings.concat(
+                "-fx-font-size: ", pane.heightProperty().multiply(0.025).asString()
+        ));
     }
 
-    protected void setByoyomi(int totalNumber, int number, int time) {
-        if(totalNumber == 0) {
-            byoyomiLabel.setVisible(false);
+    private void setByoyomi(int totalNumber, int number, int time) {
+        if (totalNumber == 0) {
             byoyomi.setVisible(false);
         } else {
             byoyomi.setText(number + "/" + totalNumber + " time periods à " + time + " s left");
             byoyomi.toFront();
         }
+
+        byoyomi.styleProperty().bind(Bindings.concat(
+                "-fx-font-size: ", pane.heightProperty().multiply(0.025).asString()
+        ));
     }
 
     public void onNewGameClick(ActionEvent event) throws IOException {
@@ -93,7 +134,7 @@ public class winnerMaskController {
     protected void setSize(double width, double height) {
         pane.setPrefHeight(height);
         pane.setPrefWidth(width);
-        pane.setMinSize(400, 300);
+        pane.setMinSize(300, 300);
     }
 
     protected double getWidth() {
