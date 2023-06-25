0426d001dd16cd43f9695ece9ec953fbfcc1d71f
vasilev14 <ivvveselinov@gmail.com>
2023-06-25 18:00:05 +0300

    starting with tests

---
+++ Go_Gruppe1/Test/com/example/go_gruppe1/controller/winnerMaskControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.go_gruppe1.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class winnerMaskControllerTest {
+
+    @Test
+    void initiateDisplay() {
+    }
+}
