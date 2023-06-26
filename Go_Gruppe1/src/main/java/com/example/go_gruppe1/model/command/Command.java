95683333e0d43225cafb53a20cac9ae4620c5f1c
BigBurneXX <128181584+BigBurneXX@users.noreply.github.com>
2023-06-26 23:16:04 +0200

    some code refinement

--- Go_Gruppe1/src/main/java/com/example/go_gruppe1/model/command/Command.java
+++ Go_Gruppe1/src/main/java/com/example/go_gruppe1/model/command/Command.java
@@ -1,8 +1,9 @@
 package com.example.go_gruppe1.model.command;
 
 public interface Command {
-    boolean execute();
+    int execute();
     void undo();
 
     SimpleBoard getBoard();
+    String getDescription();
 }
