93d5966a2b69f239bcc1a7cf2360218d0134e3f4
BigBurneXX <128181584+BigBurneXX@users.noreply.github.com>
2023-07-14 13:34:00 +0200

    some code/test refinement

--- Go_Gruppe1/Test/com/example/go_gruppe1/model/command/SimpleBoardTest.java
+++ Go_Gruppe1/Test/com/example/go_gruppe1/model/command/SimpleBoardTest.java
@@ -70,5 +70,69 @@ public class SimpleBoardTest {
         assertEquals(1, board.getTrapped(Color.WHITE));
         assertEquals(0, board.getTrapped(Color.BLACK));
     }
+
+    @Test
+    public void testCalcScores() {
+        // Set up the initial state for testing
+        double komi = 0.5;
+        board.setStone(1, 1, Color.WHITE);
+        board.setStone(0, 1, Color.BLACK);
+        board.setStone(1, 0, Color.BLACK);
+        board.setStone(1, 2, Color.BLACK);
+        board.setStone(2, 1, Color.BLACK);
+        board.calcScores(komi);
+
+        // Perform assertions to check the expected behavior
+        assertEquals(6, board.getTotal(Color.BLACK));
+        assertEquals(0.5, board.getTotal(Color.WHITE));
+    }
+/*
+    @Test
+    public void testIsExclusivelySurrounded() {
+        // Set up the initial state for testing
+        int row = 0;
+        int col = 0;
+        Color color = Color.BLACK;
+        boolean expectedResult = true;
+
+        // Perform the actual method call
+        boolean result = board.isExclusivelySurrounded(row, col, color);
+
+        // Perform assertions to check the expected behavior
+        assertEquals(expectedResult, result);
+        // Add more assertions as needed
+    }
+
+    @Test
+    public void testCheckNeighbours() {
+        // Set up the initial state for testing
+        int row = 0;
+        int col = 0;
+        Color color = Color.BLACK;
+        boolean expectedResult = true;
+
+        // Perform the actual method call
+        boolean result = yourObject.checkNeighbours(row, col, color);
+
+        // Perform assertions to check the expected behavior
+        assertEquals(expectedResult, result);
+        // Add more assertions as needed
+    }
+
+    @Test
+    public void testFindLibertyArea() {
+        // Set up the initial state for testing
+        int row = 0;
+        int col = 0;
+        ArrayList<Position> libertyArea = new ArrayList<>();
+        boolean[][] visited = new boolean[size][size];
+        // Set up the visited matrix and libertyArea as needed
+
+        // Perform the actual method call
+        yourObject.findLibertyArea(row, col, libertyArea, visited);
+
+        // Perform assertions to check the expected behavior
+        // Add assertions based on the expected behavior of the method
+    }*/
 }
 
